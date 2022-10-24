bcdc8dcac26fe2cc15f6efa1518024946dcd1158
khurshid88 <shamsun.com@gmail.com>
2022-10-25 00:19:12 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/pdp/pattern_setstate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pdp.pattern_setstate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import '../model/post_model.dart';
+import '../services/http_service.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isLoading = false;
+  List<Post> items = [];
+
+  void _apiPostList() async {
+    setState(() {
+      isLoading = true;
+    });
+    var response = await Network.GET(Network.API_LIST, Network.paramsEmpty());
+    setState(() {
+      if (response != null) {
+        items = Network.parsePostList(response);
+      } else {
+        items = [];
+      }
+      isLoading = false;
+    });
+  }
+
+  void _apiPostDelete(Post post) async {
+    setState(() {
+      isLoading = true;
+    });
+    var response = await Network.DEL(
+        Network.API_DELETE + post.id.toString(), Network.paramsEmpty());
+    setState(() {
+      if (response != null) {
+        _apiPostList();
+      }
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _apiPostList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("setState"),
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (ctx, index) {
+              return itemOfPost(items[index]);
+            },
+          ),
+          isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : const SizedBox.shrink(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget itemOfPost(Post post) {
+    return Slidable(
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        dismissible: DismissiblePane(onDismissed: () {}),
+        children: [
+          SlidableAction(
+            onPressed: (BuildContext context) {},
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+            icon: Icons.edit,
+            label: "Update",
+          )
+        ],
+      ),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            // An action can be bigger than the others.
+            onPressed: (BuildContext context) {
+              _apiPostDelete(post);
+            },
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
+      ),
+      child: Container(
+        padding: const EdgeInsets.only(left: 20,right: 20, top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              post.title.toUpperCase(),
+              style: const TextStyle(
+                  color: Colors.black),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              post.body,
+              style: const TextStyle(
+                  color: Colors.black54),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
